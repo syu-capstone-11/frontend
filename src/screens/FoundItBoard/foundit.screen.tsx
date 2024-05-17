b7b40fb2a6d261a4db71c3d2c2e928535a2d5a4a
jiyun <jiyyy0720@naver.com>
2024-05-17 14:55:20 +0900

    pull 하기 전에 push

--- src/screens/FoundItBoard/foundit.screen.tsx
+++ src/screens/FoundItBoard/foundit.screen.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { useNavigation } from "@react-navigation/native";
-import { View, Text } from 'react-native';
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {View, Text} from 'react-native';
 
 export const FoundItBoard = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    return (
-        <View>
-            <Text>물건 찾아 주기 게시판</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>물건 찾아 주기 게시판</Text>
+    </View>
+  );
+};
